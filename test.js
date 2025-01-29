bb5e28ff1012c1d702b86f2f59d08febdf3448dd
unknown <priyankabasti4444@gmail.com>
2025-01-29 23:00:44 +0530

    created new js file to practice js from scratch -varible

---
+++ test.js
@@ -0,0 +1,14 @@
+//Variables
+let a=10;
+console.log(a)
+
+let age=20;
+console.log(age)
+console.log(age++)
+console.log(age+2)
+
+var salary=80000;
+console.log(salary)
+
+const page=30;
+console.log(age)
