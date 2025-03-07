a2776852c84148b3a6163d86d814e68b95456ba3
unknown <priyankabasti4444@gmail.com>
2025-03-07 23:32:00 +0530

    printing methods-console,print,alert

---
+++ interview.js
@@ -0,0 +1,10 @@
+let a=10;
+let b=20;
+let sum=a+b;
+let diff=a-b;
+let mul=a*b;
+let div=a/b;
+let answer = "the answer is " +sum + " " +diff+ " " +mul+ " " +div;
+console.log(answer);
+print(answer);
+alert(answer);
