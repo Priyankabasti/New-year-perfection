92c7adea8c30244a25f6a44b02f8bd06989d2835
unknown <priyankabasti4444@gmail.com>
2025-06-14 22:09:58 +0530

    Practice session

--- JS/first.js
+++ JS/first.js
@@ -1,5 +1,3 @@
-console.log("hello from priya");
-
 //var(gobal scope) let const(block scope)
 let fullName = "priyanka";
 let age = 24;
@@ -7,7 +5,7 @@ const a = null;
 let b = undefined;
 const isFollow =true;
 
-console.log(age , fullName, a, isFollow, b);
+// console.log(age , fullName, a, isFollow, b);
 
 //data types
 //1. Primitive -- number, string, boolean, null, undefined,BigInt, symbol
@@ -20,31 +18,32 @@ let isFollow1 = null;
 let isFollow2 = undefined;
 let isFollow3 = BigInt(100);
 let isFollow4 = Symbol("priyanka");
-console.log(num, price, isLogin, name, address, isFollow, isFollow1);
+// console.log(num, price, isLogin, name, address, isFollow, isFollow1);
 
 
 //= operator
-console.log (4 == '4') //true
-console.log (4 === '4') //false
+// console.log (4 == '4') //true
+// console.log (4 === '4') //false
 
-console.log(isNaN('hello'))
-console.log(isNaN(NaN))
-console.log(isNaN(undefined))
-console.log(isNaN('123'))
-console.log(isNaN(null))
+// console.log(isNaN('hello'))
+// console.log(isNaN(NaN))
+// console.log(isNaN(undefined))
+// console.log(isNaN('123'))
+// console.log(isNaN(null))
 
 //array
 let fruits = ["apple" , "banana", "orange","jackfruit"];
 
 for (let i =0;i< fruits.length;i++){
-  console.log(fruits[i])
+  // console.log(fruits[i])
 }
 
 //Map
 const numbers = [1,2,3,4,5];
 
 const double =numbers.map(num => num*3)
-console.log(double);
+// console.log(double);
+// console.log(numbers);
 
 //types of output
 //1. console.log
@@ -53,11 +52,11 @@ console.log(double);
 //4. alert
 //5. prompt
 
-console.log("this console.log output")
-window.print(5+4);
-window.alert("this is from alert")
-window.prompt("this is from prompt")
-document.write("hello from document write")
+// console.log("this console.log output")
+// window.print(5+4);
+// window.alert("this is from alert")
+// window.prompt("this is from prompt")
+// document.write("hello from document write")
 
 //6. confirm
 //7. console.error
@@ -70,7 +69,15 @@ document.write("hello from document write")
 //14. console.clear
 //15. console.time
 
+//String Methods
+let str = "hello world";
+
+//For in
+
+//For of
+
+//While
+
+//Do While
+
 //function
-for (let i = 0; i < 5; i++) {
-  console.count();
-}
