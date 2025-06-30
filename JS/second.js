6e088df6d605166b79a09187a6b029dde96f867c
unknown <priyankabasti4444@gmail.com>
2025-06-30 22:32:58 +0530

    did small task on my own and felt good

--- JS/second.js
+++ JS/second.js
@@ -9,3 +9,7 @@ document.getElementById("intro").innerHTML="hello world!"
 function change(){
 document.getElementById("main-img").src="./images/about-us2.png";
 }
+
+//Change Style with JavaScript
+//Change the background color of a <div> with id="box" to blue.
+document.getElementById("box").style.background="red"
