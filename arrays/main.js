1b190eac4b1011169ff8406a68983a44ae99d6d1
unknown <priyankabasti4444@gmail.com>
2025-08-18 22:27:26 +0530

    small changes

--- arrays/main.js
+++ arrays/main.js
@@ -1,2 +1,25 @@
-let arr=[1,2,3,4];
-console.log(arr.length);
+let arr=["pink","purple","black"];
+console.log(arr[1]);
+
+let fruits = ["Apple", "Banana", "Mango"];
+fruits.push("grapes")
+console.log(fruits);
+
+let numbers = [10, 20, 30, 40];
+numbers.shift();
+console.log(numbers);
+
+let fruit = ["Orange", "Banana", "Kiwi"];
+let check=fruit.includes("Banana");
+console.log(check);
+
+// Write a program to print all elements of this array using forEach:
+let number = [1, 2, 3, 4, 5];
+let result= number.forEach
+
+//reverse an array
+let arr1 = [1, 2, 3, 4, 5];
+arr1.reverse();
+console.log(arr1);
+
+//
